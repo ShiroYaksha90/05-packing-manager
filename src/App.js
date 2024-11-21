efbb3b83f81593fbde22179e4bb8bc1187a49859
ShiroYaksh90 <aljazaeribasim@gmail.com>
2024-11-21 18:45:31 +0300

    Refactor App component to manage packing list state and improve item handling

--- src/App.js
+++ src/App.js
@@ -1,74 +1,83 @@
 import './App.css';
-import {useState} from 'react'
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
+import { useState } from 'react'
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
 
-const Logo = ()=>{
+const Logo = () => {
   return (
     <>
       <h1>🏖️ Packing Manager 👜</h1>
     </>
   )
 }
-const Form = ()=>{
-  const [description,setDescription]= useState("");
-  const [quantity,setQuantity]=useState(1)
-  const handelSubmit =(e)=>{
+const Form = ({ onAddItems }) => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1)
+
+
+  const handelSubmit = (e) => {
     e.preventDefault()
-    if(!description) return;
-    const newItem = {description,quantity,packed:false,id:Date.now()}
-    initialItems.push(newItem)
-    console.log(newItem)
-    console.log(initialItems)
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    onAddItems(newItem)
     setDescription('')
     setQuantity(1)
   }
-  return(
+  return (
     <form className="add-form" onSubmit={handelSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select value={quantity}onChange={(e)=>setQuantity(Number(e.target.value))}>{Array.from({length:20},(_,i)=>i+1).map(num=><option value={num} key={num}>{num}</option>)}</select>
-      <input type='text' placeholder='item...' value={description} onChange={(e)=>setDescription(e.target.value)}/>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>{Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}</select>
+      <input type='text' placeholder='item...' value={description} onChange={(e) => setDescription(e.target.value)} />
       <button>Add</button>
     </form>
   )
 }
-const PackingList = ()=>{
-  return(
+const PackingList = ({ items, onDeleteItem }) => {
+  return (
     <div className="list">
-    <ul>
-      {initialItems.map((item)=>
-        <Item item={item} key={item.id}/>)}
-    </ul>
+      <ul>
+        {items.map((item) =>
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />)}
+      </ul>
     </div>
   )
 }
 
-const Item = ({item})=>{
-  return( 
+const Item = ({ item, onDeleteItem }) => {
+  return (
     <li>
-      <span style={item.packed ? {textDecoration:"line-through"}:{}}>{item.quantity} {item.description}</span>
-      <button style={{fontSize:"4.5rem",color:"red"}}>&times;</button>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>{item.quantity} {item.description}</span>
+      <button style={{ fontSize: "4.5rem", color: "red" }} onClick={() => onDeleteItem(item.id)}>&times;</button>
     </li>
   )
 }
-const Stats = ()=>{
-  return(
+const Stats = () => {
+  return (
     <footer className='stats'>
       <em>👜 You have X items on your list, and you already packed X (X%)</em>
     </footer>
   )
 }
 
-export default function App(){
+export default function App() {
+  const [items, setItems] = useState([])
+  const handelAddItems = (item) => {
+    setItems((items) => [...items, item])
+  }
+
+  const handelDeletedItem = (id) => {
+    setItems(items => items.filter(item => item.id !== id))
+  }
+
   return (
     <div className="app">
-      <Logo/>
-      <Form/>
-      <PackingList/>
-      <Stats/>
+      <Logo />
+      <Form onAddItems={handelAddItems} />
+      <PackingList items={items} onDeleteItem={handelDeletedItem} />
+      <Stats />
     </div>
   )
 };
